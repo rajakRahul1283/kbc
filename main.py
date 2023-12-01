c77375d9dfd50a53b0f5cd2c11dd3311a68022ad
RahulRajak2 <20622709-RahulRajak2@users.noreply.replit.com>
2023-12-01 04:32:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+questionone="what is you name:"
+print(questionone)
+option=["rahul","monu","raju","om"]
+print(option[0])
+print(option[1])
+print(option[2])
+print(option[3])
+
+
+a=input("Answer the Question Your Time Start now :")
+if(a==option[0]):
+   
+  
+  print("you are right and you won 1000")
+
+
+else:
+  print("you are wrong and you can go")
+
+
+questionsecond= "what is your age:"
+print(questionsecond)
+option1=[23,34,19,20]
+print(option1[0])
+print(option1[1])
+print(option1[2])
+print(option1[3])
+b=input("Answer the Question Your Time Start now:")
+if(b==option1[1]):
+  print("you are right and you won 3000")
+else:
+  print("you are wrong and you can go")
+
+questionthird= "what is the capital of India:"
+print(questionthird)
+captial=["delhi","mumbai","kolkata","pune"]
+print(captial[0])
+print(captial[1])
+print(captial[2])
+print(captial[3])
+c=input("Answer the Question Your Time Start now:")
+if(c==captial[0]):
+  print("you won 5000 indian rupees")
+
+else:
+  print("You can go your home")
+
+questionfour="Which is the biggest river in India"
+print(questionfour)
+river=["Ganga", "Yamuna", "Bharambutra","tapti"]
+print(river[0])
+print(river[1])
+print(river[2])
+print(river[3])
+d=input("Answer the Question Your Time Start now:")
+if (d==river[0]):
+  print("you won a 10000 indian rupees")
+
+else:
+  print("Your answer is incorrect you can not play forward")
